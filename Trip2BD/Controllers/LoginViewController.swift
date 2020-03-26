25e6c3506c84749e83ad3196a290d69b044fd1fe
Md. Mehedi Hasan Akash <macMehi@labs-mac.local>
2020-03-27 04:29:26 +0600

    Add Login operation codes with modified view

--- Trip2BD/Controllers/LoginViewController.swift
+++ Trip2BD/Controllers/LoginViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import Alamofire
+import QuartzCore
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var rememberMeSwitch: UISwitch!
     
     var check = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextField.layer.cornerRadius = 6
+        emailTextField.layer.masksToBounds = true
+        passwordTextField.layer.cornerRadius = 6
+        passwordTextField.layer.masksToBounds = true
+        loginButton.layer.cornerRadius = 8
+        rememberMeSwitch.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
 
         // Do any additional setup after loading the view.
     }
@@ -27,16 +37,79 @@ class LoginViewController: UIViewController {
         let userPassword = passwordTextField.text
         
         // Checking if the fields are empty
+        if(userEmail == ""){
+            self.displayAlertMessage("Email field is empty!", "Please enter your email.")
+        } else if(userPassword == ""){
+            self.displayAlertMessage("Password field is empty!", "Please enter the password.")
+        } else if(userEmail != "" && userPassword != ""){
+            //check if the email is valid
+            if(isValidEmail(email: userEmail!)){
+                let loginDictionary = ["email" : userEmail!,
+                                       "password" : userPassword!,
+                                       "api_key" : API.API_key] as [String : Any]
+                
+                //Calling the API
+                Alamofire.request(API.baseURL + "/tourist/Login", method: .post, parameters: loginDictionary).validate().responseJSON {
+                    response in
+                    
+                    //Showing response in log
+                    print(response)
+                    
+                    if let result = response.result.value {
+                        let JSON = result as! NSDictionary
+                        print("Success = ")
+                        print(JSON["success"]!)
+                        self.check = JSON["success"] as! Bool
+                        print(self.check)
+                    }
+                    
+                    if(!self.check){
+                        self.displayAlertMessage("Invalid Email ID or Password", "Please enter correct email and password.")
+                    } else if(self.check){
+                        do{
+                            let loginResponseData = try JSONDecoder().decode(Tourist.self, from: response.data!)
+                            let successMesage = loginResponseData.success
+                            //let isVerified = loginResponseData.data[0].is_verified
+                            let id = loginResponseData.data[0].id
+                            //print(isVerified!)
+                            print(id!)
+                            
+                            print(successMesage as Any)
+                            
+                            if(successMesage == true){
+                                //if(isVerified == 0){
+                                print("I am there!")
+                                self.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil); // .presentingViewController?.presentingViewController?
+                                print("Passed there")
+                                    //self.dismiss(animated: true, completion: nil)
+                                    //self.performSegue(withIdentifier: "", sender: self)
+                                //}
+                            }
+                        } catch{
+                            print("Error while parsing JSON")
+                        }
+                    }
+                }
+            } else{
+                displayAlertMessage("Invalid email", "Your email address is not valid")
+            }
+        }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //function to display alert message
+    func displayAlertMessage(_ title: String, _ userMessage: String){
+        let userAlert = UIAlertController(title: title, message: userMessage, preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        userAlert.addAction(okAction)
+        self.present(userAlert, animated: true, completion: nil)
     }
-    */
+    
+    //function to verify email address
+    func isValidEmail(email: String) -> Bool{
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
 
 }
